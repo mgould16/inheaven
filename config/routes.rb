8f22e10726cc9ac452fdf810fece46663cc0d229
Mark Gould <m.gould16@gmail.com>
2019-05-16 11:22:37 +0100

    added FAQ

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'pages/home'
   get 'pages/about'
   get 'pages/contact'
+  get 'pages/faq'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
